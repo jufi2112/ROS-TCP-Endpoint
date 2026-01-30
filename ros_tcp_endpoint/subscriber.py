d6a0497c7505a613c559f69e10d04d52055e5d1f
Julien Fischer <jufi2112@gmail.com>
2026-01-30 14:10:01 +0100

    changed QoS reliability policy to best effort

--- ros_tcp_endpoint/subscriber.py
+++ ros_tcp_endpoint/subscriber.py
@@ -43,7 +43,9 @@ class RosSubscriber(RosReceiver):
         self.tcp_server = tcp_server
         self.queue_size = queue_size
 
-        qos_profile = QoSProfile(depth=queue_size)
+        qos_profile = QoSProfile(depth=queue_size,
+                                 reliability=QoSReliabilityPolicy.BEST_EFFORT
+        )
 
         # Start Subscriber listener function
         self.subscription = self.create_subscription(
